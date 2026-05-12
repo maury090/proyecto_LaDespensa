44a3bb3353ed87e37ae3922f23f912f400b6ebeb
maury090 <129112305+maury090@users.noreply.github.com>
2026-05-11 21:15:13 -0400

    muestra de elementos
    se muestran productos ingresados a la base de datos en sus respectivas vistas

--- supermercado/views.py
+++ supermercado/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render
 from .models import Producto, Categoria
 
-# Create your views here.
 
-def index (request): 
-
-    #llamar elementos producto y cateria de la base de datos 
-    try: 
-        categoria = Categoria.objects.get(nombre='Alimentos')
-        productos = Producto.objects.filter(categoria=categoria)
-
-    except:
-        productos = Producto.objects.all()
-        
+def index(request):
+    # Obtener productos marcados para mostrar en el index
     productos = Producto.objects.filter(mostrar_en_index=True)
-    return render (request, 'index.html', {'productos': productos})
+    
+    # Aplicar 10% de descuento a cada producto
+    for producto in productos:
+        producto.precio_con_descuento = int(producto.precio * 0.9)  # 10% descuento
+    
+    return render(request, 'index.html', {'productos': productos})
 
 def conocenos (request):
     return render (request, 'conocenos.html')
@@ -26,10 +22,40 @@ def crearUsuario (request):
     return render (request, 'crearUsuario.html')
 
 def alimentos (request):
-    return render (request, 'alimentos.html')
+    #llama al id de la categoria
+    productos = Producto.objects.filter(categoria_id=4)  
+    
+    #aplicacion de descuento
+    for producto in productos:
+        if producto.mostrar_en_index:
+            producto.precio_con_descuento = int(producto.precio * 0.9)
+        else:
+            producto.precio_con_descuento = None
+
+    return render (request, 'alimentos.html', {'productos': productos})
 
 def aseo (request):
-    return render (request, 'aseo.html')
+    productos = Producto.objects.filter(categoria_id=5)
+
+    for producto in productos:
+        if producto.mostrar_en_index:
+            producto.precio_con_descuento = int(producto.precio * 0.9)
+        else:
+            producto.precio_con_descuento = None
+
+    return render (request, 'aseo.html', {'productos': productos})
 
 def bebestibles (request):
-    return render (request, 'bebestibles.html')
+    #llamado de ids
+    productos = Producto.objects.filter(
+        categoria_id__in=[2, 3]  
+    )
+
+    #descuento del 10% 
+    for producto in productos:
+        if producto.mostrar_en_index:
+            producto.precio_con_descuento = int(producto.precio * 0.9)
+        else:
+            producto.precio_con_descuento = None  
+    
+    return render(request, 'bebestibles.html', {'productos': productos})
